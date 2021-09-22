83dd88bdc322d43d3fb3d039a035d489161681e5
mchadds <cdundassmith@gmail.com>
2021-09-22 17:44:28 -0600

    defined reference values for User to be incoporated in Todo

--- src/index.js
+++ src/index.js
@@ -10,23 +10,34 @@ const randomId = () => Math.floor(Math.random() * 1000).toString(36);
 const Todo = types
   .model({
     name: types.optional(types.string, ""),
-    done: types.optional(types.boolean, false)
+    done: types.optional(types.boolean, false),
+    user: types.maybe(types.reference(types.late(() => User)))
   })
   .actions(self => { // self = the object being constructed when an instance of your model is created
     // set the name of the attribute
     function setName(newName) {
       self.name = newName;
     }
+    // set the user for the task
+    function setUser(user) {
+      if (user === "") {
+        self.user = null
+      }
+      else {
+        self.user = user;
+      }
+    }
     // toggle the todo to complete or incomplete
     function toggle() {
       self.done = !self.done;
     }
 
-    return { setName, toggle };
+    return { setName, setUser, toggle };
   });
 
 // entity model for user
 const User = types.model({
+  id: types.identifier,
   name: types.optional(types.string, "")
 });
 
@@ -34,7 +45,7 @@ const User = types.model({
 const RootStore = types
   .model({
     users: types.map(User),
-    todos: types.optional(types.map(Todo), {})
+    todos: types.map(Todo)
   })
   .views(self => ({
     // returns number of incomplete tasks
@@ -44,6 +55,12 @@ const RootStore = types
     // returns number of complete tasks
     get completedCount() {
       return values(self.todos).filter(todo => todo.done).length;
+    },
+    // declared model view of todos that are either complete or incomplete (based on method parameter input)
+    // so you do not have to use filtering everytime like in the gets above
+    // model view is declard a function over the properties (first argument) of the model declaration
+    getTodosWhereDoneIs(done) {
+      return values(self.todos).filter(todo => todo.done === done);
     }
   }))
   .actions(self => {
@@ -58,11 +75,24 @@ const RootStore = types
 // create new model instance and pass it a snapshot - this requires all store references to be updated 
 // if used in react components 
 const store = RootStore.create({
-  users: {},
+  users: {
+    "1": {
+        id: "1",
+        name: "mweststrate"
+    },
+    "2": {
+        id: "2",
+        name: "mattiamanzati"
+    },
+    "3": {
+        id: "3",
+        name: "johndoe"
+    }
+  },
   todos: {
     "1": {
-      name: "Eat a cake",
-      done: true
+        name: "Eat a cake",
+        done: true
     }
   }
 });
@@ -79,6 +109,19 @@ const store = RootStore.create({
 //   }
 // });
 
+// component to select the user for the todo
+const UserPickerView =observer(props => (
+  <select
+    value={props.user ? props.user.id : ""}
+    onChange={e => props.onChange(e.target.value)}
+  >
+    <option value="">-none-</option>
+    {values(props.store.users).map(user => (
+      <option value={user.id}>{user.name}</option>
+    ))}
+  </select>
+));
+// componenet for each todo on the list
 const TodoView = observer(props => (
   <div>
     <input
@@ -91,22 +134,29 @@ const TodoView = observer(props => (
       value={props.todo.name}
       onChange={e => props.todo.setName(e.target.value)}
     />
+    <UserPickerView
+      user={props.todo.user}
+      store={props.store}
+      onChange={userId => props.todo.setUser(userId)}
+    />
   </div>
 ));
 
+// displays the number of complete and incomplete tasks
 const TodoCounterView = observer(props => (
   <div>
     {props.store.pendingCount} pending, {props.store.completedCount} completed
   </div>
 ))
 
+// app componenet that includes the list of todos and the todos counter
 const AppView = observer(props => (
   <div>
     <button onClick={e => props.store.addTodo(randomId(), "New Task")}>
       Add Task
     </button>
     {values(props.store.todos).map(todo => (
-      <TodoView todo = {todo} />
+      <TodoView store={props.store} todo={todo} />
     ))}
     <TodoCounterView store={props.store} />
   </div>
